428264a8738c61492680aa5b445f7e816ab24e7a
piaocreative <johnpiao171019@gmail.com>
2021-09-13 19:55:38 -0400

    Changed the main js

---
+++ js/main.js
@@ -0,0 +1,107 @@
+$( document ).ready( function() {
+    
+    if ( $( '.logos' ).length ) {
+        $( '.logos' ).slick({
+            infinite: true,
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            dots: false,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                }
+            ]
+        });
+    }  
+
+    if ( $( '#events' ).length ) {
+        $( '#events' ).slick({
+            infinite: false,
+            slidesToShow: 2.8,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+    } 
+    
+    if ( $( '#new-products' ).length ) {
+
+        $( '#new-products' ).slick({
+            infinite: true,
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 1000,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+
+    }
+
+} );
